47af8766647d79d011e274ec240e7ef443a8106e
zscaiosi <zscaio.si@gmail.com>
2017-12-10 18:53:42 -0200

    First commit

---
+++ TouristGuideApp/app/src/main/java/com/example/caio/touristguideapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.caio.touristguideapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
