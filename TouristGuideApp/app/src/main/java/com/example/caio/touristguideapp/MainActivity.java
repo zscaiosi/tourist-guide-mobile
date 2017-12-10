e8f35e7a0d193c350fc26074a5f5f39860474c14
zscaiosi <zscaio.si@gmail.com>
2017-12-10 19:22:20 -0200

    First Activity

--- TouristGuideApp/app/src/main/java/com/example/caio/touristguideapp/MainActivity.java
+++ TouristGuideApp/app/src/main/java/com/example/caio/touristguideapp/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.caio.touristguideapp;
 
+import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> placesList = null;
+    private ArrayAdapter<String> arrAdapter = null;
+    private Button btnAddPlaces = null;
+    private ListView lvPlaces = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvPlaces = (ListView) findViewById(R.id.lvPlaces);
+        placesList = new ArrayList<String>();
+        btnAddPlaces = (Button) findViewById(R.id.btnAdd);
+        arrAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, placesList);
+        lvPlaces.setAdapter(arrAdapter);
+
+        btnAddPlaces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                placesList.add("Museu Nacional");
+                arrAdapter.notifyDataSetChanged();
+
+            }
+        });
+
     }
+
+
 }
